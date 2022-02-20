ece169d5bb89b27e6e85f79c1a2bcd76c85ebb4c
nicolekevans <81535902+nicolekevans@users.noreply.github.com>
2022-02-19 20:06:33 -0600

    WordBreaks and null text tests

--- src/nevans/wordWrapper/WordWrapper.java
+++ src/nevans/wordWrapper/WordWrapper.java
@@ -2,25 +2,48 @@ package nevans.wordWrapper;
 
 public class WordWrapper {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public String wrap(String text, int maxLineLength) {
+	
+	public static String wrap(String text, int maxLineLength) {
+		
+		//If a null value is submitted for the text, a blank string is returned
+		if (text==null) {
+			return "";
+		}
+		
+		
+		// Initializes the maximum length for a line break.
+		int lineBreak = maxLineLength;
 		StringBuilder sb = new StringBuilder(text);
-		int start = maxLineLength;
-		for(int i =1; start<sb.length(); i++) {
-				System.out.println("before:" + sb);
-				System.out.println("sb.length=" +sb.length() +", start="+ start + ", i="+ i);
-				sb.insert(start, '\n');
-				System.out.println("after:"+ sb);
-				start = start+maxLineLength+i;
-				
+		
+		
+		//while the next lineBreak value is less than the length of the text
+		//the location of the next lineBreak is found
+		while(lineBreak<sb.length()) {
+			
+			//if lineBreak ends on a space, the space is replaced with a \n
+				if(sb.charAt(lineBreak)== ' ') {
+					sb.replace(lineBreak, lineBreak+1, "\n");
+					lineBreak = lineBreak+maxLineLength;
+				}
+				else {
+					int lastSpace= lineBreak+1;
+					   //Checks to see if the character after the maxLength is a space
+					   // If so, replaces that with a \n
+					   if(sb.charAt(lineBreak+1)== ' ') {
+						   sb.insert(lineBreak+1, "\n");
+						   sb.deleteCharAt(lineBreak+2);
+					   }
+					   //else if it lands on a character, the last space is found and 
+					   //is replaced with a \n
+					   else {
+						   StringBuilder findSpace = new StringBuilder (sb.substring(0, lineBreak));
+						   lastSpace =	findSpace.lastIndexOf(" ");
+						   sb.replace(lastSpace, lastSpace+1, "\n");
+						
+				       }
+			         lineBreak= lastSpace+maxLineLength;
+				}
 			}
-		
-		
-		
 		return sb.toString();
 	}
 
--- src/nevans/wordWrapper/tests/WordWrapperTest.java
+++ src/nevans/wordWrapper/tests/WordWrapperTest.java
@@ -2,22 +2,54 @@ package nevans.wordWrapper.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
 import nevans.wordWrapper.WordWrapper;
 
 class WordWrapperTest {
+	WordWrapper wordWrap = new WordWrapper();
+	
 
 	@Test
-	void testWrapInsertsLineBreaks() {
+	void testWrapInsertsLineBreaksOnWordBreaks() {
+		//Given maxLineLength and a sample text
 		int maxLineLength = 10;
-		String text = "abcdefghijklmnopqrstuvwxyz";
-		String expected ="abcdefghij\nklmnopqrst\nuvwxyz";
-		WordWrapper wordWrap = new WordWrapper();
-		String newWrap = wordWrap.wrap(text, maxLineLength);
-		System.out.println(newWrap);
-		assertEquals(expected, newWrap);
+		String text = "The quick brown fox jumps over the lazy dog.";
+		//When parsed through the wrap function
+		String newWrap = WordWrapper.wrap(text, maxLineLength);
+		//Then: Outputs a new text with desired maxLineLenths AND breaks on work Breaks
 		
+		System.out.println(newWrap);
+		String expected ="The quick\nbrown fox\njumps over\nthe lazy\ndog.";
+		assertEquals(expected,newWrap);
 	}
+	
+	@Test
+	void testIfNoTextIsProvided() {
+		//Given maxLineLength and an null text
+		int maxLineLength = 12;
+		String text = null;
+		//When parsed through the wrap function
+		String newWrap = WordWrapper.wrap(text, maxLineLength);
+		//Then: Outputs a new text with desired maxLineLenths AND breaks on work Breaks
+		System.out.println(newWrap);
+		String expected ="";
+		assertEquals(expected,newWrap);
+	}
+	
+	@Test
+	void testIfTextIsShorterThanMaxLineLength(){
+		//Given maxLineLength and short text;
+		int maxLineLength = 12;
+		String text = "Hi mom!";
+		//When parsed through the wrap function
+		String newWrap = WordWrapper.wrap(text, maxLineLength);
+		//Then: Outputs a new text with desired maxLineLenths AND breaks on work Breaks
+		System.out.println(newWrap);
+		String expected ="Hi mom!";
+		assertEquals(expected,newWrap);
+	}
+	
 
 }
